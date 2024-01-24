3e4afbf8128fff50651119f976313e8c65346a52
CodingGaby <macma.cube1@gmail.com>
2024-01-24 11:36:42 -0600

    CitiesController agregado /Cities

---
+++ Agro.webSolution/Agro.web/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Agro.web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agro.web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<City> cities { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) //Base hace que el constructor herede de el constructor de la clase padre
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<City>().HasIndex(x => x.Name).IsUnique();
+        }
+        
+        //ORM
+        //Objeto   -> Solo una cosa  -> City
+        //Tabla BD -> Muchos objetos -> Cities
+    }
+}
--- Agro.webSolution/Agro.web/Data/Entities/City.cs
+++ Agro.webSolution/Agro.web/Data/Entities/City.cs
@@ -1,9 +1,16 @@
-﻿namespace Agro.web.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agro.web.Data.Entities
 {
     public class City
     {
         //Creado por JGGO
         public int Id { get; set; }
+
+        //Data anotations de el atributo Name
+        [Required(ErrorMessage = "El campo {0} es obligatorio")] //Mostrar el error en la vista
+        [Display(Name = "Ciudad")] //Cambiar el nombre al campo Name
+        [MaxLength(100,ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")] //Se establece que Name no puede tener mas de 100 caracteres
         public string Name { get; set; } = null!;
     }
 }
