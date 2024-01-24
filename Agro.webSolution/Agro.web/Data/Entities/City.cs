32a527dae544536819f33ccd256d71bd596d0cf6
CodingGaby <macma.cube1@gmail.com>
2024-01-24 10:15:56 -0600

    Primer Commit

---
+++ Agro.webSolution/Agro.web/Data/Entities/City.cs
@@ -0,0 +1,9 @@
+﻿namespace Agro.web.Data.Entities
+{
+    public class City
+    {
+        //Creado por JGGO
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
